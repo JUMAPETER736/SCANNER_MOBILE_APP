68a4a98a755018be2f4c5e5834e37132384fca31
JUMAPETER736 <jumapeter736@gmail.com>
2024-09-08 13:07:44 +0200

    Making a Code for Automatic Student Subjects creation in the Firebase

--- lib/Main_Screen/StudentSubjects.dart
+++ lib/Main_Screen/StudentSubjects.dart
@@ -28,85 +28,153 @@ class StudentSubjects extends StatelessWidget {
           ),
         ),
         padding: const EdgeInsets.all(16.0),
-        child: StreamBuilder<DocumentSnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('classes')
-              .doc(studentClass)
-              .snapshots(),
+        child: FutureBuilder<void>(
+          future: _initializeDefaultSubjects(studentClass),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             }
-            if (!snapshot.hasData || !snapshot.data!.exists) {
-              return Center(child: Text('No subjects found for class $studentClass.'));
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
             }
+            return StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('StudentSubjects')
+                  .doc(studentClass)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || !snapshot.data!.exists) {
+                  return Center(child: Text('No subjects found for class $studentClass.'));
+                }
 
-            var data = snapshot.data!.data() as Map<String, dynamic>;
-            var subjects = data['subjects'];
+                var data = snapshot.data!.data() as Map<String, dynamic>;
+                var subjects = data['Subjects'] as List<dynamic>?;
 
-            if (subjects is List<dynamic>) {
-              // Sort subjects alphabetically
-              List<Map<String, dynamic>> sortedSubjects = subjects
-                  .map((subject) => subject is Map<String, dynamic> ? subject : {'name': subject, 'grade': 'N/A'})
-                  .toList();
+                if (subjects != null) {
+                  List<Subject> subjectList = subjects
+                      .map((subjectData) => Subject.fromMap(subjectData as Map<String, dynamic>))
+                      .toList();
 
-              sortedSubjects.sort((a, b) => (a['name'] as String).compareTo(b['name'] as String));
+                  subjectList.sort((a, b) => a.name.compareTo(b.name));
 
-              return ListView.separated(
-                itemCount: sortedSubjects.length,
-                separatorBuilder: (context, index) => Divider(color: Colors.blueAccent, thickness: 1.5),
-                itemBuilder: (context, index) {
-                  var subject = sortedSubjects[index];
-                  String subjectName = subject['name'] ?? 'N/A';
-                  String grade = subject['grade'] ?? 'N/A';
-
-                  return Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[50],
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 4,
-                          offset: Offset(2, 2),
+                  return ListView.separated(
+                    itemCount: subjectList.length,
+                    separatorBuilder: (context, index) => Divider(color: Colors.blueAccent, thickness: 1.5),
+                    itemBuilder: (context, index) {
+                      var subject = subjectList[index];
+                      return Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.blue[50],
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 4,
+                              offset: Offset(2, 2),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    margin: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(16),
-                      title: Text(
-                        subjectName,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent,
+                        margin: const EdgeInsets.symmetric(vertical: 4.0),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(16),
+                          title: Text(
+                            subject.name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          trailing: Text(
+                            'Grade: ${subject.grade}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          tileColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          leading: Icon(Icons.book, color: Colors.blueAccent),
                         ),
-                      ),
-                      trailing: Text(
-                        'Grade: $grade',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent,
-                        ),
-                      ),
-                      tileColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      leading: Icon(Icons.book, color: Colors.blueAccent),
-                    ),
+                      );
+                    },
                   );
-                },
-              );
-            } else {
-              return Center(child: Text('Subjects data is NOT  in the expected format.'));
-            }
+                } else {
+                  return Center(child: Text('Subjects data is not in the expected format.'));
+                }
+              },
+            );
           },
         ),
       ),
+    );
+  }
+
+  Future<void> _initializeDefaultSubjects(String className) async {
+    final subjects = {
+      'FORM 1': [
+        Subject(name: 'Mathematics'),
+        Subject(name: 'English'),
+        Subject(name: 'Science'),
+        Subject(name: 'Social Studies'),
+        Subject(name: 'Chichewa'),
+      ],
+      'FORM 2': [
+        Subject(name: 'Mathematics'),
+        Subject(name: 'English'),
+        Subject(name: 'Physics'),
+        Subject(name: 'Chemistry'),
+        Subject(name: 'Biology'),
+      ],
+    };
+
+    final subjectList = subjects[className] ?? [];
+
+    final classRef = FirebaseFirestore.instance.collection('StudentSubjects').doc(className);
+
+    // Get current document data
+    DocumentSnapshot docSnapshot = await classRef.get();
+    if (docSnapshot.exists) {
+      // Update existing document if subjects do not exist
+      final existingSubjects = (docSnapshot.data() as Map<String, dynamic>)['Subjects'] as List<dynamic>?;
+      if (existingSubjects == null || existingSubjects.isEmpty) {
+        await classRef.update({
+          'Subjects': subjectList.map((subject) => subject.toMap()).toList(),
+        });
+      }
+    } else {
+      // Create new document with subjects
+      await classRef.set({
+        'Subjects': subjectList.map((subject) => subject.toMap()).toList(),
+      });
+    }
+  }
+}
+
+class Subject {
+  final String name;
+  final String grade;
+
+  Subject({required this.name, this.grade = 'N/A'});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'grade': grade,
+    };
+  }
+
+  factory Subject.fromMap(Map<String, dynamic> map) {
+    return Subject(
+      name: map['name'] ?? 'N/A',
+      grade: map['grade'] ?? 'N/A',
     );
   }
 }
